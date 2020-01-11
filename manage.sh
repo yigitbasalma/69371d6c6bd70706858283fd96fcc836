dcc343eee95886f29c35037e2bf11feafab7d575
yigit.basalma <yigit.basalma@gmail.com>
2020-01-12 01:43:31 +0300

    Tüm süreci yönetmek için gerekli script yazıldı.

--- manage.sh
+++ manage.sh
@@ -8,6 +8,28 @@ NC="\033[0m"
 
 # Environments
 SETUP_PREFIX="kubernetes-setup-for"
+read -r -d '' HELP <<- EOM
+Help for ${0};
+    --operation:    Operation name. [infra-setup, build, k8s-setup, deploy, bundle-deploy]
+        for infra-setup
+            Infrastructure setup for lab. Run vagrant up, create k8s cluster with 1 master 1 node, get k8s credential.
+        for build
+            Run docker build in docker folder. Your application code must be in docker/app folder.
+            Parameters:
+                --application-name:     Application name for docker image.
+            Optional parameters:
+                --docker-registry:      Docker registry address for docker pull command. You must be logged in to remote registry.
+        for k8s-setup
+            Prepare your application to deploy k8s. Your all yaml files belongs to your application prepare dynamically.
+            Parameters:
+                --application-name:     Application name for deployment.
+                --k8s-namespace:        Namespace for your environment.
+                --k8s-services:         Application services. Syntax: "protocol:port:target-port;..."
+                --k8s-env-variables:    Environment variables for application container. Syntax: "key:value;..."
+                --k8s-image:            Image name for your application container.
+            Optional parameters:
+                --db:                   Database software name for your application. [mysql]
+EOM
 
 function setup_infra {
     bash setup.sh
@@ -21,7 +43,7 @@ function setup_infra {
 
 function build_application {
     cd docker && \
-        docker build -t ${APPLICATION_NAME}:latest . 2>&1 > /dev/null
+        docker build -t ${APPLICATION_NAME}:latest . 1> /dev/null
     if [ $? -ne 0 ]
     then
         echo -e "Docker image build. ${RED}[ERROR]${NC}"
@@ -31,7 +53,7 @@ function build_application {
 
     if [ ${#REGISTRY} -ne 0 ]
     then
-        docker build -t ${REGISTRY}/${APPLICATION_NAME}:latest . 2>&1 > /dev/null && \
+        docker build -t ${REGISTRY}/${APPLICATION_NAME}:latest . 1> /dev/null && \
             docker push ${REGISTRY}/${APPLICATION_NAME}:latest
 
         if [ $? -ne 0 ]
@@ -48,10 +70,10 @@ function build_application {
 }
 
 function k8s_setup {
-    kubectl create ns ${K8S_NAMESPACE} 2>&1 > /dev/null
+    kubectl create ns ${K8S_NAMESPACE} 1> /dev/null
     if [[ ${#DATABASE} -ne 0 && -d kubernetes/${DATABASE} ]]
     then
-        kubectl apply -n ${K8S_NAMESPACE} -f kubernetes/${DATABASE}/ 2>&1 > /dev/null
+        kubectl apply -n ${K8S_NAMESPACE} -f kubernetes/${DATABASE}/ 1> /dev/null
 
         if [ $? -ne 0 ]
         then
@@ -61,7 +83,7 @@ function k8s_setup {
         echo -e "Database setup for ${DATABASE}. ${GREEN}[OK]${NC}"
     fi
 
-    mkdir ${SETUP_PREFIX}-${K8S_NAMESPACE} 2>&1 > /dev/null && \
+    mkdir ${SETUP_PREFIX}-${K8S_NAMESPACE} 1> /dev/null && \
         j2 kubernetes/application/service.yaml > ${SETUP_PREFIX}-${K8S_NAMESPACE}/services.yaml && \
         j2 kubernetes/application/deployment.yaml > ${SETUP_PREFIX}-${K8S_NAMESPACE}/deployment.yaml
 
@@ -71,6 +93,17 @@ function k8s_setup {
         exit 1
     fi
     echo -e "Kubernetes setup completed for ${K8S_NAMESPACE}. ${GREEN}[OK]${NC}"
+}
+
+function deploy {
+    kubectl apply -n ${K8S_NAMESPACE} -f ${SETUP_PREFIX}-${K8S_NAMESPACE}/ 1> /dev/null
+
+    if [ $? -ne 0 ]
+    then
+        echo -e "Application deployment for ${APPLICATION_NAME}. ${RED}[ERROR]${NC}"
+        exit 1
+    fi
+    echo -e "Application deployment for ${APPLICATION_NAME}. ${GREEN}[OK]${NC}"
 }
 
 while [[ $# -gt 0 ]]
@@ -121,6 +154,7 @@ while [[ $# -gt 0 ]]
         ;;
         --db)  # mysql, postgres
         DATABASE="$2"
+        export DATABASE="$2"
         shift
         shift
         ;;
@@ -149,7 +183,53 @@ case ${OPERATION} in
     fi
     k8s_setup
     ;;
-    *)
+    deploy)
+    if [ ${#K8S_NAMESPACE} -eq 0 ]
+    then
+        echo -e "${RED}[ERROR]${NC} You should give a namespace with --k8s-namespace parameter."
+        exit 1
+    fi
+    if [[ ! -d ${SETUP_PREFIX}-${K8S_NAMESPACE} ]]
+    then
+        echo -e "${RED}[ERROR]${NC} You should run k8s-setup option before this."
+        exit 1
+    fi
+    deploy
+    ;;
+    bundle-deploy)
+    # infra-setup
+    setup_infra
 
+    # build
+    if [ ${#APPLICATION_NAME} -eq 0 ]
+    then
+        echo -e "${RED}[ERROR]${NC} You should give an application name with --application-name parameter."
+        exit 1
+    fi
+    build_application
+
+    # k8s-setup
+    if [[ ${#K8S_NAMESPACE} -eq 0 || ${#K8S_SERVICES} -eq 0 || ${#K8S_ENV_VARIABLES} -eq 0 || ${#K8S_IMAGE} -eq 0 || ${#APPLICATION_NAME} -eq 0 ]]
+    then
+        echo -e "${RED}[ERROR]${NC} You should give following parameters. --application-name, --k8s-namespace, --k8s-services, --k8s-env-variables, --k8s-image."
+        exit 1
+    fi
+    k8s_setup
+
+    # deploy
+    if [ ${#K8S_NAMESPACE} -eq 0 ]
+    then
+        echo -e "${RED}[ERROR]${NC} You should give a namespace with --k8s-namespace parameter."
+        exit 1
+    fi
+    if [[ ! -d ${SETUP_PREFIX}-${K8S_NAMESPACE} ]]
+    then
+        echo -e "${RED}[ERROR]${NC} You should run k8s-setup option before this."
+        exit 1
+    fi
+    deploy
+    ;;
+    *)
+    echo -e "${HELP}"
     ;;
 esac
