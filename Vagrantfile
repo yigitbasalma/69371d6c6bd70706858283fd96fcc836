fcf761c1cbe139a9a6fac8058d8bfca1e6747678
yigit.basalma <yigit.basalma@gmail.com>
2020-01-11 17:40:45 +0300

    Kurulum scripti oluşturuldu.

--- Vagrantfile
+++ Vagrantfile
@@ -11,12 +11,12 @@ Vagrant.configure("2") do |config|
 
     config.vm.define "k8s-master" do |master|
         master.vm.box = IMAGE_NAME
-        master.vm.network "private_network", ip: "192.168.50.20"
+        master.vm.network "public_network", ip: "192.168.56.20", bridge: "enp0s3"
         master.vm.hostname = "k8s-master"
         master.vm.provision "ansible" do |ansible|
             ansible.playbook = "kubernetes-setup/master-playbook.yml"
             ansible.extra_vars = {
-                node_ip: "192.168.50.10",
+                node_ip: "192.168.56.20",
             }
         end
     end
@@ -24,12 +24,12 @@ Vagrant.configure("2") do |config|
     (1..N).each do |i|
         config.vm.define "node-#{i}" do |node|
             node.vm.box = IMAGE_NAME
-            node.vm.network "private_network", ip: "192.168.50.#{i + 30}"
+            node.vm.network "public_network", ip: "192.168.56.#{i + 30}", bridge: "enp0s3"
             node.vm.hostname = "node-#{i}"
             node.vm.provision "ansible" do |ansible|
                 ansible.playbook = "kubernetes-setup/node-playbook.yml"
                 ansible.extra_vars = {
-                    node_ip: "192.168.50.#{i + 30}",
+                    node_ip: "192.168.56.#{i + 30}",
                 }
             end
         end
---
+++ setup.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+# Color codes
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+NC="\033[0m"
+
+# Environments
+LOG_FILE=/tmp/vagrant_up.$(date "+%Y.%m.%d-%H.%M.%S").log
+
+function vagrant_executable {
+    which vagrant 2>&1 > /dev/null
+    if [ $? -ne 0 ]
+    then
+        echo -e "Check vagrant executable ${RED}[ERROR]${NC}"
+        exit 1
+    fi
+    echo -e "Check vagrant executable ${GREEN}[OK]${NC}"
+}
+
+function vagrant_scp_plugin_install {
+    vagrant plugin install vagrant-scp 2>&1 > /dev/null
+    if [ $? -ne 0 ]
+    then
+        echo -e "Vagrant scp plugin install ${RED}[ERROR]${NC}"
+        exit 1
+    fi
+    echo -e "Vagrant scp plugin install ${GREEN}[OK]${NC}"
+}
+
+function ansible_executable {
+    which ansible 2>&1 > /dev/null
+    if [ $? -ne 0 ]
+    then
+        echo -e "Check ansible executable ${RED}[ERROR]${NC}"
+        exit 1
+    fi
+    echo -e "Check ansible executable ${GREEN}[OK]${NC}"
+}
+
+function docker_executable {
+    which docker 2>&1 > /dev/null
+    if [ $? -ne 0 ]
+    then
+        echo -e "Check docker executable ${RED}[ERROR]${NC}"
+        exit 1
+    fi
+    echo -e "Check docker executable ${GREEN}[OK]${NC}"
+}
+
+function docker_usability {
+    docker ps 2>&1 > /dev/null
+    if [ $? -ne 0 ]
+    then
+        echo -e "Check docker command usability ${RED}[ERROR]${NC}"
+        exit 1
+    fi
+    echo -e "Check docker command usability ${GREEN}[OK]${NC}"
+}
+
+function prerequisites {
+    vagrant_executable
+    vagrant_scp_plugin_install
+    ansible_executable
+    docker_executable
+    docker_usability
+}
+
+function vagrant_up {
+    vagrant up 2>&1 >> ${LOG_FILE}
+    if [ $? -ne 0 ]
+    then
+        echo -e "Vagrant up ${RED}[ERROR]${NC}"
+        echo -e "Please see the log file. Path: ${LOG_FILE}"
+        exit 1
+    fi
+    vagrant scp k8s-master:/home/vagrant/.kube/config /tmp/kube_config 2>&1 >> ${LOG_FILE}
+    echo -e "Vagrant up ${GREEN}[OK]${NC}"
+}
+
+function kubernetes_access_setup {
+    # If user already has a kubernetes config, append to new one
+    if [ -d ${HOME}/.kube ]
+    then
+        cat /tmp/kube_config >> ${HOME}/.kube/config
+    else
+        mkdir ${HOME}/.kube && cat /tmp/kube_config >> ${HOME}/.kube/config
+    fi
+    if [ $? -ne 0 ]
+    then
+        echo -e "Shipping kube config ${RED}[ERROR]${NC}"
+        exit 1
+    fi
+    echo -e "Shipping kube config ${GREEN}[OK]${NC}"
+}
+
+function setup {
+    # Run prerequisites
+    echo -e "Running checks before jump to the action."
+    prerequisites
+
+    echo -e "\nJumping to the action. The first step is running the vagrant up command. This could take a few minutes. You can follow all processes belongs to this step with a log file named  ${LOG_FILE}."
+    # Run vagrant process
+    vagrant_up
+    kubernetes_access_setup
+}
+
+# Start setup
+setup
